489358d9fa448be928c967909cbd0daa7c62663c
Demon6590 <dima.chernytin@gmail.com>
2025-12-01 11:28:03 +0300

    попытка намбер ту
    Вторая редакция кода

--- db_postgres.sql
+++ db_postgres.sql
@@ -8,14 +8,16 @@ CREATE TABLE table_positions
 
 CREATE TABLE table_emails
 (
-    id    SERIAL NOT NULL PRIMARY KEY,
-    email TEXT   NOT NULL CHECK (email LIKE '%@%')
+    id        SERIAL NOT NULL PRIMARY KEY,
+    email     TEXT   NOT NULL CHECK (email LIKE '%@%'),
+    person_id INT    NOT NULL
 );
 
 CREATE TABLE table_telephones
 (
     id        SERIAL NOT NULL PRIMARY KEY,
-    telephone TEXT   NOT NULL
+    telephone TEXT   NOT NULL,
+    person_id INT    NOT NULL
 );
 
 CREATE TABLE table_discounts
@@ -34,7 +36,10 @@ CREATE TABLE table_product_types
 CREATE TABLE table_manufacturers
 (
     id           SERIAL NOT NULL PRIMARY KEY,
-    manufacturer TEXT   NOT NULL CHECK (manufacturer <> '')
+    manufacturer TEXT   NOT NULL CHECK (manufacturer <> ''),
+    address      TEXT   NOT NULL CHECK ( address <> '' ),
+    telephone    TEXT   NOT NULL,
+    email        TEXT   NOT NULL CHECK (email LIKE '%@%')
 );
 
 CREATE TABLE table_order_formats
@@ -45,17 +50,8 @@ CREATE TABLE table_order_formats
 
 CREATE TABLE table_matrics
 (
-    id        SERIAL NOT NULL PRIMARY KEY,
-    unit_type TEXT   NOT NULL CHECK (unit_type <> '')
-);
-
--- Level 2: Depend on Level 1
-CREATE TABLE table_price_per_unit_types
-(
-    id             SERIAL  NOT NULL PRIMARY KEY,
-    price_per_unit NUMERIC NOT NULL CHECK (price_per_unit >= 0.0),
-    unit_type_id   INT     NOT NULL,
-    FOREIGN KEY (unit_type_id) REFERENCES table_matrics (id)
+    id     SERIAL NOT NULL PRIMARY KEY,
+    matric TEXT   NOT NULL CHECK (matric <> '')
 );
 
 CREATE TABLE table_price_change_history
@@ -67,48 +63,53 @@ CREATE TABLE table_price_change_history
 
 CREATE TABLE table_products
 (
-    id                 SERIAL  NOT NULL PRIMARY KEY,
-    product_name       TEXT    NOT NULL CHECK (product_name <> ''),
-    product_type_id    INT     NOT NULL,
-    price_id           INT     NOT NULL,
-    price_per_unit_id  INT     NOT NULL,
-    expiration_date    DATE    NULL,
-    manufacturing_date DATE    NOT NULL,
-    product_quantity   INT     NOT NULL CHECK (product_quantity >= 0),
-    primecost          NUMERIC NOT NULL CHECK (primecost >= 0.0),
-    manufacturer_id    INT     NOT NULL,
+    id                      SERIAL  NOT NULL PRIMARY KEY,
+    product_name            TEXT    NOT NULL CHECK (product_name <> ''),
+    product_type_id         INT     NOT NULL,
+    price_sale              NUMERIC NOT NULL,
+    price_change_history_id INT     NULL,
+    matric_id               INT     NOT NULL,
+    expiration_date         DATE    NULL,
+    manufacturing_date      DATE    NOT NULL,
+    product_quantity        INT     NOT NULL CHECK (product_quantity >= 0),
+    prime_cost               NUMERIC NOT NULL CHECK (prime_cost >= 0.0),
+    manufacturer_id         INT     NOT NULL,
+    FOREIGN KEY (matric_id) REFERENCES table_matrics (id),
     FOREIGN KEY (manufacturer_id) REFERENCES table_manufacturers (id),
-    FOREIGN KEY (price_id) REFERENCES table_price_change_history (id),
-    FOREIGN KEY (price_per_unit_id) REFERENCES table_price_per_unit_types (id),
+    FOREIGN KEY (price_change_history_id) REFERENCES table_price_change_history (id),
     FOREIGN KEY (product_type_id) REFERENCES table_product_types (id)
+);
+
+CREATE TABLE table_persons
+(
+    id         SERIAL NOT NULL PRIMARY KEY,
+    first_name TEXT   NOT NULL CHECK (first_name <> ''),
+    last_name  TEXT   NOT NULL CHECK (last_name <> ''),
+    patronymic TEXT   NULL CHECK (patronymic <> ''),
+    sex        sex,
+    email_id     INT    NOT NULL,
+    telephone_id INT    NOT NULL,
+    FOREIGN KEY (email_id) REFERENCES table_emails (person_id),
+    FOREIGN KEY (telephone_id) REFERENCES table_telephones (person_id)
 );
 
 CREATE TABLE table_sellers
 (
     id             SERIAL  NOT NULL PRIMARY KEY,
-    first_name     TEXT    NOT NULL CHECK (first_name <> ''),
-    last_name      TEXT    NOT NULL CHECK (last_name <> ''),
-    patronymic     TEXT    NULL CHECK (patronymic <> ''),
+    person_id      INT     NOT NULL,
     positions_id   INT     NOT NULL,
     date_admission DATE    NOT NULL,
-    sex            sex,
-    salary         NUMERIC NOT NULL CHECK (salary >= 0.0),
+    FOREIGN KEY (person_id) REFERENCES table_persons (id),
     FOREIGN KEY (positions_id) REFERENCES table_positions (id)
 );
 
 CREATE TABLE table_clients
 (
     id           SERIAL NOT NULL PRIMARY KEY,
-    first_name   TEXT   NOT NULL CHECK (first_name <> ''),
-    last_name    TEXT   NOT NULL CHECK (last_name <> ''),
-    patronymic   TEXT   NULL CHECK (patronymic <> ''),
-    email_id     INT    NOT NULL,
-    telephone_id INT    NOT NULL,
-    sex          sex,
-    discount_id  INT    NOT NULL,
+    person_id    INT    NOT NULL,
+    discount_id  INT    NULL,
     subscription BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (email_id) REFERENCES table_emails (id),
-    FOREIGN KEY (telephone_id) REFERENCES table_telephones (id),
+    FOREIGN KEY (person_id) REFERENCES table_persons (id),
     FOREIGN KEY (discount_id) REFERENCES table_discounts (id)
 );
 
@@ -142,3 +143,4 @@ CREATE TABLE table_sales
     seller_id INT    NOT NULL,
     FOREIGN KEY (order_id) REFERENCES table_orders (id),
     FOREIGN KEY (seller_id) REFERENCES table_sellers (id)
+);
